91a7271a3476d33984f1adbc34f767b6100845be
DMQQ <kozakdamian73@gmail.com>
2026-03-10 19:54:46 +0100

    Move app update to native side of expo-updates to prevent issues with blackscreen or stuck splash, fix db issues

--- src/service/useInit.ts
+++ src/service/useInit.ts
@@ -1,10 +1,8 @@
-import * as Updates from "expo-updates";
 import { useEffect, useState } from "react";
 import { Platform } from "react-native";
 import { loadAsync } from "expo-font";
 
 export default function useInit() {
-  const [isUpdating, setIsUpdating] = useState(false);
   const [isLoaded, setIsLoaded] = useState(Platform.OS !== "ios");
 
   useEffect(() => {
@@ -15,42 +13,5 @@ export default function useInit() {
     });
   }, []);
 
-  const { isUpdateAvailable, isUpdatePending, checkError, downloadError, isChecking } = Updates.useUpdates();
-
-  console.log("[useInit] isUpdateAvailable:", isUpdateAvailable);
-  console.log("[useInit] isUpdatePending:", isUpdatePending);
-
-  useEffect(() => {
-    if (checkError) {
-      console.error("[useInit] Update check error:", checkError);
-    }
-  }, [checkError]);
-
-  useEffect(() => {
-    if (downloadError) {
-      console.error("[useInit] Update download error:", downloadError);
-      setIsUpdating(false);
-    }
-  }, [downloadError]);
-
-  useEffect(() => {
-    if (isUpdateAvailable) {
-      setIsUpdating(true);
-      Updates.fetchUpdateAsync();
-    }
-  }, [isUpdateAvailable]);
-
-  useEffect(() => {
-    if (isUpdatePending) {
-      Updates.reloadAsync({
-        reloadScreenOptions: {
-          backgroundColor: "#000",
-          fade: true,
-          image: require("../../assets/images/adaptive-icon.png"),
-        },
-      });
-    }
-  }, [isUpdatePending]);
-
-  return { isLoaded: isLoaded, isUpdating: isUpdating || isChecking };
+  return { isLoaded: isLoaded, isUpdating: false };
 }
